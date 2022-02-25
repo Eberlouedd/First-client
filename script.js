e4abeb5e536e73a1127c7ffa3022eb663b727001
Eberlouedd <kyacini@yahoo.fr>
2022-02-26 00:22:03 +0100

    update

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const auto = document.querySelector('.auto');
-const account = document.querySelector('.account');
-const analys = document.querySelector('.analys');
-const pAuto = document.querySelector('.auto p');
-const dAuto = document.querySelector('.auto-project');
-const pAccount = document.querySelector('.account p');
-const pAnalys = document.querySelector('.analys p');
-
-const selectAuto = () => {
-    const timeLine = gsap.timeline({paused: true});
-
-    timeLine.to(account, {display: 'none', duration: 0});
-    timeLine.to(analys, {display: 'none', duration: 0});
-    timeLine.to(dAuto, {display: 'flex', duration: 0})
-    timeLine.to(pAuto, {display: 'none', duration: 0});
-    timeLine.to(auto, {width: '100%', duration: 1}, '+=0.5');
-    timeLine.to('.auto-project div', {opacity: 1, stagger: 0.5});
-    timeLine.play();
-
+option = {
+    root: null,
+    rootmargin: 0,
+    threshold: 0.5
 }
+
+const handleIntersect = (entries, observer) => {
+    entries.forEach(entry => {
+        console.log(entry.intersectionRatio);
+        if(entry.intersectionRatio >= 0.5){
+            entry.target.classList.add('reveal');
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+observer = new IntersectionObserver(handleIntersect, option);
+
+observer.observe(document.querySelector('.hidden'));
